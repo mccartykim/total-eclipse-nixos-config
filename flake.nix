b46f587006a1ad64ff454e2e5699f56869dcd008
Kimberly McCarty <mccartykim@zoho.com>
2024-03-16 23:19:36 -0400

    Argh, why is it so hard to find nixpkgs urls...

--- flake.nix
+++ flake.nix
@@ -4,17 +4,11 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    stable-nixpkgs.url = "github:nixos/nixpkgs/nixos-stable";
 
     # Home manager
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # TODO: Add any other flake you might need
-    # hardware.url = "github:nixos/nixos-hardware";
-
-    # Shameless plug: looking for a way to nixify your themes and make
-    # everything match nicely? Try nix-colors!
     nix-colors.url = "github:misterio77/nix-colors";
   };
 
