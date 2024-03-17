d88866d9b513d85cf642a9594cc6728c25215b3e
Kimberly McCarty <mccartykim@zoho.com>
2024-03-16 23:41:28 -0400

    Fine, no steam then

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -45,7 +45,7 @@
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
-  home.packages = with pkgs; [ steam ];
+  home.packages = with pkgs; [ ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
